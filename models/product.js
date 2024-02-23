2b1ce99de2faa94c61d8464f0e7487bcd9dbd0e3
Bharath Reddy <127417229+bharath9812@users.noreply.github.com>
2024-02-24 00:12:54 +0530

    Add files via upload

--- models/product.js
+++ models/product.js
@@ -1,1 +1,28 @@
+const mongoose = require('mongoose');
 
+// =======================================================================================================
+
+//              CREATING SCHEMA FOR OUR MODEL
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min:0
+    },
+    category: {
+        type: String,
+        lowercase:true,
+        enum: ['fruit','vegetable','dairy']
+    }
+})
+
+// =======================================================================================================
+
+//          CREating model from our schema
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product; // we can import this model on other file and use it over there
